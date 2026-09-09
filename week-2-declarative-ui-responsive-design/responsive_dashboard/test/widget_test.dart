e4026db438665799d3c6c5fd47fa64bbb4646405
Nanta <aryaputraferdiananta@gmail.com>
2026-09-09 18:31:18 +0700

    tugas 2

--- week-2-declarative-ui-responsive-design/responsive_dashboard/test/widget_test.dart
+++ week-2-declarative-ui-responsive-design/responsive_dashboard/test/widget_test.dart
@@ -1,30 +1,33 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:responsive_dashboard/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Dashboard satu kolom di layar sempit', (tester) async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    tester.view.physicalSize = const Size(400, 800);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(tester.view.reset);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpWidget(const DashboardApp());
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final cardFinder = find.byType(Card).first;
+    final width = tester.getSize(cardFinder).width;
+
+    expect(width, lessThan(700));
+  });
+
+  testWidgets('Dashboard dua kolom di layar lebar', (tester) async {
+
+    tester.view.physicalSize = const Size(1200, 800);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(tester.view.reset);
+
+    await tester.pumpWidget(const DashboardApp());
+
+    final cardFinder = find.byType(Card).first;
+    final width = tester.getSize(cardFinder).width;
+
+    expect(width, greaterThan(500));
   });
 }
